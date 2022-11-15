76e66c6b62ac204a8a2b58e6fa6eee574963a83e
Yves Brissaud <yves.brissaud@gmail.com>
2022-11-15 09:36:37 +0100

    feat: add default loca/cross targets
    Signed-off-by: Yves Brissaud <yves.brissaud@gmail.com>

--- mage/golang/golang.go
+++ mage/golang/golang.go
@@ -2,6 +2,7 @@ package golang
 
 import (
 	"context"
+	"path/filepath"
 
 	"github.com/eunomie/dague"
 	"github.com/eunomie/dague/daggers"
@@ -42,14 +43,52 @@ func (Go) Doc(ctx context.Context) error {
 	})
 }
 
-// Local compile go code to a binary ane export it
+// Local compiles go code from target and export it into dist/ folder for thelocal architecture
+func (Go) Local(ctx context.Context, target string) error {
+	return Local(ctx, types.LocalBuildOpts{
+		BuildOpts: types.BuildOpts{
+			Dir: "dist",
+			In:  target,
+			EnvVars: map[string]string{
+				"CGO_ENABLED": "0",
+				"GO11MODULE":  "auto",
+			},
+		},
+		Out: filepath.Base(target),
+	})
+}
+
+// Cross compiles go code from target and export it into dist/ for multiple architectures (linux|darwin|windows)/(amd64|arm64)
+func (Go) Cross(ctx context.Context, target string) error {
+	return Cross(ctx, types.CrossBuildOpts{
+		BuildOpts: types.BuildOpts{
+			Dir: "dist",
+			In:  target,
+			EnvVars: map[string]string{
+				"CGO_ENABLED": "0",
+				"GO11MODULE":  "auto",
+			},
+		},
+		OutFileFormat: filepath.Base(target) + "_%s_%s",
+		Platforms: []types.Platform{
+			{"linux", "amd64"},
+			{"linux", "arm64"},
+			{"darwin", "amd64"},
+			{"darwin", "arm64"},
+			{"windows", "amd64"},
+			{"windows", "arm64"},
+		},
+	})
+}
+
+// Local compiles go code to a binary ane export it
 func Local(ctx context.Context, buildOpts types.LocalBuildOpts) error {
 	return dague.RunInDagger(ctx, func(c *dagger.Client) error {
 		return daggers.LocalBuild(ctx, c, buildOpts)
 	})
 }
 
-// Cross cross compiles a go binary and export all of them
+// Cross compiles a go binary for multiple platforms and export all of them
 func Cross(ctx context.Context, buildOpts types.CrossBuildOpts) error {
 	return dague.RunInDagger(ctx, func(c *dagger.Client) error {
 		return daggers.CrossBuild(ctx, c, buildOpts)
